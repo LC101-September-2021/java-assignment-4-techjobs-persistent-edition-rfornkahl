b2b8c948c3e7978fd729d466653e60bae37e3055
rfornkahl <rwfornkahl@gmail.com>
2022-03-17 16:55:11 -0500

    Update EmployerController.java

--- src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/EmployerController.java
@@ -1,6 +1,5 @@
 package org.launchcode.techjobs.persistent.controllers;
 
-
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
