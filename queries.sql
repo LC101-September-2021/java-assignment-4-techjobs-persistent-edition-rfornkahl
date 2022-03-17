1bf219b9b2193a4109896e254b145202a63742e5
rfornkahl <rwfornkahl@gmail.com>
2022-03-16 21:47:45 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,5 +1,8 @@
 ## Part 1: Test it with SQL
-SELECT * FROM job;
+id INTEGER
+    employer VARCHAR(255)
+    name VARCHAR(255)
+    skills VARCHAR(255)
 
 ## Part 2: Test it with SQL
 SELECT name
@@ -10,8 +13,8 @@ WHERE location = "St. Louis City";
 DROP TABLE job;
 
 ## Part 4: Test it with SQL
-SELECT *
+SELECT * 
 FROM skill
-LEFT JOIN job_skills ON skill.id = job_skills.skills_id
-WHERE job_skills.jobs_Id IS NOT NULL
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
