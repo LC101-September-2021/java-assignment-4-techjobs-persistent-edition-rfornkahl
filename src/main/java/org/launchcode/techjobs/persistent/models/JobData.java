ec36181ca6419493fcaa9d67d27a2c2818ae6bc3
rfornkahl <rwfornkahl@gmail.com>
2022-03-16 22:21:23 -0500

    Update JobData.java

--- src/main/java/org/launchcode/techjobs/persistent/models/JobData.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/JobData.java
@@ -54,3 +54,38 @@ public class JobData {
         } else {
             theValue = job.getSkills().toString();
         }
+
+        return theValue;
+    }
+
+    /**
+     * Search all Job fields for the given term.
+     *
+     * @param value The search term to look for.
+     * @param allJobs The list of jobs to search.
+     * @return      List of all jobs with at least one field containing the value.
+     */
+    public static ArrayList<Job> findByValue(String value, Iterable<Job> allJobs) {
+        String lower_val = value.toLowerCase();
+
+        ArrayList<Job> results = new ArrayList<>();
+
+        for (Job job : allJobs) {
+
+            if (job.getName().toLowerCase().contains(lower_val)) {
+                results.add(job);
+            } else if (job.getEmployer().toString().toLowerCase().contains(lower_val)) {
+                results.add(job);
+            } else if (job.getSkills().toString().toLowerCase().contains(lower_val)) {
+                results.add(job);
+            } else if (job.toString().toLowerCase().contains(lower_val)) {
+                results.add(job);
+            }
+
+        }
+
+        return results;
+    }
+
+
+}
