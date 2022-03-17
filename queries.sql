cc4726b38cdcf77b5ef0409ff25ee46cd8a2b2a1
rfornkahl <rwfornkahl@gmail.com>
2022-03-16 21:34:23 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -2,9 +2,16 @@
 SELECT * FROM job;
 
 ## Part 2: Test it with SQL
-SELECT name FROM techjobs.employer;
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 ## Part 3: Test it with SQL
-DROP TABLE techjobs.job;
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT *
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_Id IS NOT NULL
+ORDER BY name ASC;
