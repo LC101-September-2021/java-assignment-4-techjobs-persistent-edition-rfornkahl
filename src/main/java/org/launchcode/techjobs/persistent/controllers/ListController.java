08b3b1a33ebbe2a655a5bc5398bee6f95d3dbabf
rfornkahl <rwfornkahl@gmail.com>
2022-03-17 20:17:11 -0500

    Removed dup dirs and updated db psswd

--- src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/ListController.java
@@ -47,6 +47,7 @@ public class ListController {
 
         model.addAttribute("employers", employerRepository.findAll());
         model.addAttribute("skills", skillRepository.findAll());
+        
         return "list";
     }
 
